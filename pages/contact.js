7c6d50c443a8e060bbb61638aac3b3f4f79ae00c
mitchbrunke <65658409+mitchbrunke@users.noreply.github.com>
2021-08-26 09:24:57 +1000

    Chelsea updates

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,18 @@
 import ContactForm from "../components/Form";
 import Hero from "../components/Hero";
+import styles from "../styles/contact.module.css";
 
 const contact = () => {
   return (
     <div>
       <Hero title="Contact" />
+      <div className={styles.contact_block}>
+        <p>
+          {" "}
+          For any enquires contact the Hervey Bay Visitor Information centre on
+          <strong>1800 811 728</strong>.
+        </p>
+      </div>
       <ContactForm />
     </div>
   );
